26155703b893cb4d27d7f84ad069eb0da81d5bf6
dadondev <dadondev03@gmail.com>
2024-10-01 18:31:36 +0500

    updated

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,7 +5,6 @@ import { getAllTests } from "../../actions";
 import { SpeedDialWithTextOutside } from "../../components/menu";
 import Modal from "../../components/modal";
 import useUserData from "../../zustand/user";
-import { loginRespI } from "../../types/types";
 import useTestsStore from "../../zustand/tests";
 import TestCard from "./components/test.card";
 import { Spinner } from "@material-tailwind/react";
@@ -13,11 +12,14 @@ import { Spinner } from "@material-tailwind/react";
 const Home = () => {
 	const { tests, giveAll } = useTestsStore();
 	const [data, setData] = useState(tests);
-	const { id, giveUser } = useUserData();
+	const { giveUser } = useUserData();
 	const [loading, setLoading] = useState(true);
 	useEffect(() => {
-		const payload: loginRespI = JSON.parse(localStorage.getItem("user") || "");
-		if (id.length !== 0 && payload.id) giveUser(payload);
+		const payload = localStorage.getItem("user");
+		if (payload && payload.length > 0) {
+			const datas = JSON.parse(payload);
+			giveUser(datas);
+		}
 		getAllTests().then((e) => {
 			giveAll(e);
 			setLoading(false);
